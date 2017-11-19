acc27f235edb451974f97da07528652a1237e596
amalakamala <amalakamala.a@gmail.com>
2017-11-19 17:24:48 -0300

    inicio

---
+++ src/components/app.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import CreateTodo from './create-todo';
+import TodosList from './todos-list';
+
+const todos = [
+	{
+	    task: 'make React tutorial',
+	    isCompleted: false
+	},
+	{
+	    task: 'eat dinner',
+	    isCompleted: true
+	}
+];
+
+
+export default class App extends React.Component {
+	render()
+}
